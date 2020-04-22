de9aa1e7ebf2a75635eba2da2a6af65e11df330f
Троицкий Максим Алексеевич <horinf@mail.ru>
2020-04-22 15:27:34 +0300

    DeepCopy

---
+++ ToolsToLive.ConcurrentCache/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ToolsToLive.ConcurrentCache
+{
+    public static class Extensions
+    {
+        /// <summary>
+        ///  Can be used to deep copy of an object. But it takes time, so probably not so good idea to use it along with fast cache.
+        /// </summary>
+        public static T DeepCopy<T>(this T source)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, source);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+    }
+}
